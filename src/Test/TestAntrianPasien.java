8c442ac814d61d3fafbad113193e3cb8d0683f45
asus <asus@MORGAN>
2018-09-25 23:15:18 +0700

    Version

---
+++ src/Test/TestAntrianPasien.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Test;
+
+import Model.AntrianPasien;
+import Model.Klinik;
+import Model.Pasien;
+
+/**
+ *
+ * @author asus
+ */
+public class TestAntrianPasien {
+
+    public static void main(String[] args) {
+        AntrianPasien.buatAntrian(
+                21,
+                9,
+                2018,
+                new Klinik("001", "anak"));
+
+        AntrianPasien.buatAntrian(
+                21,
+                9,
+                2018,
+                new Klinik("002", "saraf"));
+
+        AntrianPasien.buatAntrian(
+                21,
+                9,
+                2018,
+                new Klinik("001", "anak"));
+
+        for (int i = 0; i < AntrianPasien.daftarAntrian.size(); i++) {
+            System.out.println(AntrianPasien.daftarAntrian.get(i));
+        }
+
+        Pasien pasien1 = new Pasien();
+        pasien1.setNama("puspa");
+        pasien1.setAlamat("klaten");
+
+        Pasien pasien2 = new Pasien();
+        pasien2.setNama("adi");
+        pasien2.setAlamat("klaten");
+        Klinik a = new Klinik("001", "anak");
+        int indeksAntrian = AntrianPasien.cariAntrian(21,
+                9,
+                2018,
+                a);
+        System.out.println(indeksAntrian);
+        if (indeksAntrian >= 0) {
+            AntrianPasien.daftarAntrian.get(indeksAntrian).Mendaftar(pasien1);
+            AntrianPasien.daftarAntrian.get(indeksAntrian).Mendaftar(pasien2);
+        }
+        for (int i = 0; i < AntrianPasien.daftarAntrian.size(); i++) {
+            if (indeksAntrian >= 0) {
+                System.out.println(AntrianPasien.daftarAntrian.get(indeksAntrian).getDaftarPasienAntri().get(i).getNama());
+            }
+
+        }
+    }
+}
